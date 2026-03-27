a1d3734da44303db5f6086cff5a191d1b298ca7a
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-03-28 02:34:25 +0530

    update widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,42 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:notilog/main.dart';
+import 'package:notilog/models/notification_entry.dart';
+import 'package:notilog/services/notification_controller.dart';
+import 'package:notilog/services/notification_repository.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('renders home screen shell', (WidgetTester tester) async {
+    final controller = NotificationController(_FakeNotificationRepository());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.pumpWidget(NotificationHistoryApp(controller: controller));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Notilog'), findsOneWidget);
+    expect(find.text('Search notifications'), findsOneWidget);
+    expect(find.text('All'), findsOneWidget);
+    expect(find.text('Installed'), findsOneWidget);
+    expect(find.text('System Apps'), findsOneWidget);
+    expect(find.text('Others'), findsOneWidget);
+    expect(find.text('No notifications captured yet.'), findsOneWidget);
   });
+}
+
+class _FakeNotificationRepository extends NotificationRepository {
+  _FakeNotificationRepository();
+
+  @override
+  Future<void> initialize() async {}
+
+  @override
+  Future<void> dispose() async {}
+
+  @override
+  Future<void> drainNativeBuffer() async {}
+
+  @override
+  Future<void> pruneExpired() async {}
+
+  @override
+  List<NotificationEntry> getAllSorted() => [];
 }
